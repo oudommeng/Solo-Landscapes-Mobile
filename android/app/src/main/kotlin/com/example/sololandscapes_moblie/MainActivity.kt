c2978a9b2c7a57f7b85f0da4b1396521003cb8d3
oudommeng <oudombig007@gmail.com>
2025-09-07 14:04:35 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/sololandscapes_moblie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sololandscapes_moblie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Solo Landscapes')),
+      body: const Center(child: Text('Welcome to Solo Landscapes Mobile App')),
+    );
+  }
+}
